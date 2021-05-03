da679d2187a1cb5d72411a7005b10d88a4eb0c60
David Velez <dveleztx@gmail.com>
2021-05-03 00:36:03 -0500

    Added events to the home screen if any exist. The binary, reportapp.py can be used to POST information into memory so that it can be used to retreive via the /api/reports or refreshing the home page and seeing 'recent events'.

--- views/home.py
+++ views/home.py
@@ -1,14 +1,19 @@
+# Imports
 import fastapi
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
+# Custom Imports
+from services import report_service
 
 templates = Jinja2Templates("templates")
 router = fastapi.APIRouter()
 
 
 @router.get("/")
-def index(request: Request):
-    return templates.TemplateResponse("home/index.html", {'request': request})
+async def index(request: Request):
+    events = await report_service.get_reports()
+    data = {'request': request, 'events': events}
+    return templates.TemplateResponse("home/index.html", data)
 
 
 @router.get("/favicon.ico")
