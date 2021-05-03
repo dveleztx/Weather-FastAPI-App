f355bc852a8bdf7e67831d07aae497af7bdb3de3
David Velez <dveleztx@gmail.com>
2021-05-03 00:49:44 -0500

    Removed '/' and '/favicon.ico' from the documentation that is automatically created with fastapi by going to /docs. Added a response model for the get and post methods to provide better documentation on what fields are expected to submit and to return.

--- api/weather_api.py
+++ api/weather_api.py
@@ -22,14 +22,14 @@ async def weather(loc: Location = Depends(), units: Optional[str] = 'imperial'):
         return fastapi.Response(content="Error processing your request.", status_code=500)
 
 
-@router.get("/api/reports", name="all_reports")
+@router.get("/api/reports", name="all_reports", response_model=List[Report])
 async def reports_get() -> List[Report]:
     # await report_service.add_report("A", Location(city="San Antonio"))
     # await report_service.add_report("B", Location(city="Austin"))
     return await report_service.get_reports()
 
 
-@router.post("/api/reports", name="add_reports", status_code=201)
+@router.post("/api/reports", name="add_reports", status_code=201, response_model=Report)
 async def reports_post(report_submittal: ReportSubmittal) -> Report:
     d = report_submittal.description
     loc = report_submittal.location
--- views/home.py
+++ views/home.py
@@ -9,13 +9,13 @@ templates = Jinja2Templates("templates")
 router = fastapi.APIRouter()
 
 
-@router.get("/")
+@router.get("/", include_in_schema=False)
 async def index(request: Request):
     events = await report_service.get_reports()
     data = {'request': request, 'events': events}
     return templates.TemplateResponse("home/index.html", data)
 
 
-@router.get("/favicon.ico")
+@router.get("/favicon.ico", include_in_schema=False)
 def favicon():
     return fastapi.responses.RedirectResponse(url="/static/img/favicon.ico")
